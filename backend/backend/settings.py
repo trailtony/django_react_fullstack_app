d2a4a460ab7d386cbe470beab7fcdfaa9f8a715d
trailtony <still_running_uphill@protonmail.com>
2025-09-12 15:37:55 +0100

    Updated backend settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -142,3 +142,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
